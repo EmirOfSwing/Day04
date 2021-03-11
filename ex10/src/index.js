c84c4a9c9bb98f50088c7ed1516f507239748785
EmirOfSwing <emir.arh@gmail.com>
2021-03-11 18:53:15 +0100

    Added exercises for Day04

---
+++ ex10/src/index.js
@@ -0,0 +1,15 @@
+function main(){
+
+var myFirstName = "Emir"
+var myLastName = "Škrijelj"
+var myHomeTown = "Sarajevo"
+
+    return {
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
